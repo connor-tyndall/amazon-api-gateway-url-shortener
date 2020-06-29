7ec36a1622d29605ce7cbce7832b554590babe61
Connor Tyndall <connor.tyndall@coxautoinc.com>
2020-06-29 12:46:28 -0400

    update

--- src/app/user-info-api.js
+++ src/app/user-info-api.js
@@ -2,16 +2,17 @@ import axios from 'axios';
 import auth from './auth';
 
 
-export default{  
+export default{
     getUserInfo(){
         var jwtToken = auth.auth.getSignInUserSession().getAccessToken().jwtToken;
+        localStorage.setItem("cognitoIdentityToken", jwtToken);
         const USERINFO_URL = 'https://'+auth.auth.getAppWebDomain() + '/oauth2/userInfo';
         var requestData = {
             headers: {
                 'Authorization': 'Bearer '+ jwtToken
             }
         }
-        return axios.get(USERINFO_URL, requestData).then(response => { 
+        return axios.get(USERINFO_URL, requestData).then(response => {
             return response.data;
         });
     }
